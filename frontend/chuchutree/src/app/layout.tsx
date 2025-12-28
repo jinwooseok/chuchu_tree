cd9fb39ad4ad3703eb33119bb936a1cb6f21c4b0
imnamgi <happynj2697@gmail.com>
2025-12-28 14:31:12 +0900

    Conf: tanstackquery setting

--- frontend/chuchutree/src/app/layout.tsx
+++ frontend/chuchutree/src/app/layout.tsx
@@ -1,11 +1,12 @@
-import type { Metadata } from "next";
-import "./globals.css";
+import type { Metadata } from 'next';
+import './globals.css';
+import Providers from '@/lib/providers';
 
 export const metadata: Metadata = {
-  title: "ChuChuTree",
-  description: "ChuChuTree Algorithm Calendar",
+  title: 'ChuChuTree',
+  description: 'ChuChuTree Algorithm Calendar',
   icons: {
-    icon: "/icon.png",
+    icon: '/icon.png',
   },
 };
 
@@ -16,7 +17,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <Providers>{children}</Providers>
+      </body>
     </html>
   );
 }
