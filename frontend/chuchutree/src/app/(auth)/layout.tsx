718c7895831ada618da56cad774c0931fba5c83d
imnamgi <happynj2697@gmail.com>
2026-01-04 14:41:34 +0900

    Feat: sign-in page UI

--- frontend/chuchutree/src/app/(auth)/layout.tsx
+++ frontend/chuchutree/src/app/(auth)/layout.tsx
@@ -3,5 +3,5 @@ export default function AuthLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  return <div className="bg-background flex min-h-screen items-center justify-center">{children}</div>;
+  return <div className="bg-background flex h-screen w-screen items-center justify-center p-4">{children}</div>;
 }
--- frontend/chuchutree/src/app/(auth)/sign-in/page.tsx
+++ frontend/chuchutree/src/app/(auth)/sign-in/page.tsx
@@ -1,7 +1,40 @@
+import { SocialWays } from '@/shared/constants/auth';
+import Image from 'next/image';
+
+type SocialWay = (typeof SocialWays)[number];
+
+const SocialLoginButton = (props: SocialWay) => {
+  return (
+    <button className="hover:bg-background/50 border-innerground-darkgray relative flex w-80 cursor-pointer items-center justify-between rounded border px-2 py-2 text-sm">
+      <Image src={`/socialicon/${props.en}_light.svg`} alt={`social icon ${props.en}_light`} width={16} height={16} className="absolute left-2 h-6 w-6" />
+      <div className="flex-1 text-center">{props.kr}로 시작하기</div>
+    </button>
+  );
+};
+
 export default function SignIn() {
   return (
-    <div className="flex w-screen justify-center bg-red-100">
-      <div>로그인</div>
+    <div className="bg-innerground-white flex h-full w-full flex-col rounded-xl p-4">
+      <div className="flex items-center gap-2">
+        <Image src="/logo/test_logo.png" alt="logo" width={16} height={16} className="h-6 w-6" />
+        <div>ChuChuTree</div>
+      </div>
+      <div className="flex flex-1 flex-col items-center justify-center">
+        <div className="flex flex-col gap-2">
+          <div className="mb-4">
+            <div className="font-semibold">나만의 알고리즘 캘린더</div>
+            <div className="text-muted-foreground">ChuChuTree 로그인</div>
+          </div>
+          {SocialWays.map((way) => (
+            <SocialLoginButton key={way.en} {...way} />
+          ))}
+        </div>
+        <div className="text-muted-foreground mt-4 flex cursor-default gap-1 text-xs">
+          <span className="hover:text-foreground/50">이용약관</span>
+          <span>|</span>
+          <span className="hover:text-foreground/50">개인정보처리방침</span>
+        </div>
+      </div>
     </div>
   );
 }
