928d44fbb7becf5e5d50c503749baf60f048ae08
imnamgi <happynj2697@gmail.com>
2026-01-14 14:55:22 +0900

    Feat: middleware 404검증로직 추가 + (main)/layout user정보 직접호출 방식으로 수정

--- frontend/chuchutree/src/middleware.ts
+++ frontend/chuchutree/src/middleware.ts
@@ -12,26 +12,37 @@ export async function middleware(request: NextRequest) {
 
   if (protectedPaths.includes(pathname)) {
     if (!accessToken) {
+      console.log('[Middleware] access토큰 없음');
       return NextResponse.redirect(new URL('/sign-in', request.url));
     }
     // access_token이 있는 경우, 유효성 검증
     // local환경: rewrites에 의해 https://chuchu-tree-dev.duckdns.org/api/v1/bj-accounts/me로 프록시
     // 배포 dev 환경: 상대 경로이므로 현재 도메인 기준 https://chuchu-tree-dev.duckdns.org/api/v1/bj-accounts/me 요청 후 Nginx가 이를 FastAPI:8002로 라우팅
     // 배포 production 환경: 상대 경로이므로 https://chuchu-tree.duckdns.org/api/v1/bj-accounts/me 요청 후 Nginx가 FastAPI로 FastAPI:8001(추정)로 라우팅
+
     try {
-      const verifyResponse = await fetch(`/api/v1/bj-accounts/me`, {
+      const backendUrl = process.env.NEXT_PUBLIC_BACKEND_URL || request.nextUrl.origin;
+      const verifyUrl = `${backendUrl}/api/v1/bj-accounts/me`;
+
+      const verifyResponse = await fetch(verifyUrl, {
         headers: {
           Cookie: `access_token=${accessToken}${refreshToken ? `; refresh_token=${refreshToken}` : ''}`,
         },
       });
 
       if (verifyResponse.ok) {
+        console.log('[Middleware] verifyResponse ok');
         return NextResponse.next();
       }
-
+      // 404는 백준 계정 미연동일 가능성이 있으므로 Layout에서 처리
+      if (verifyResponse.status === 404) {
+        console.log('[Middleware] 404 response, let layout handle it');
+        return NextResponse.next();
+      }
       // 401 에러 - 토큰 만료
       if (verifyResponse.status === 401 && refreshToken) {
         // 공통 함수 사용
+        console.log('[Middleware] 토큰2개있음');
         const { success, newCookies } = await refreshAccessToken(refreshToken, '');
 
         if (success && newCookies) {
@@ -48,17 +59,19 @@ export async function middleware(request: NextRequest) {
         }
       }
 
+      console.log('[Middleware] refreshToken failed 혹은 401, 404말고 다른에러:', verifyResponse);
+
       // 재발급 실패
       const response = NextResponse.redirect(new URL('/sign-in', request.url));
       response.cookies.delete('access_token');
       response.cookies.delete('refresh_token');
       return response;
     } catch (error) {
-      console.error('[Middleware] Error:', error);
+      console.error('[Middleware] anothor Error (fetch 실패):', error);
       return NextResponse.next();
     }
   }
-
+  console.log('[Middleware] 검증필요없는 페이지 (sign-in)');
   return NextResponse.next();
 }
 
